308109cfed1fd998a05941852a4edfcae1195342
aisteur <aisteu13@gmail.com>
2024-05-08 18:18:21 +0300

    new

---
+++ 04-javascript/uzduotis 8.1/8.1.js
@@ -0,0 +1,7 @@
+console.log('Aistė', '28m.\n Noriu pakeisti savo specializaciją');
+console.log(' Su Nemunu \n Pateka saulė.\n Su Nemunu \n Leidžias toli. \n Prie Nemuno \n Mano pasaulis, \n Prie Nemuno \n Mano šalis.');
+console.log('***\n* *\n***');
+console.log('*\n**\n***');
+console.log(' LDK Vyriausias raštininkas Antanas Mykolas Pacas mirė 1774 m., bet ir po jo mirties žmona Teresė darė didelę įtaką Jieznui.\n Antanas Mykolas Pacas savo valdymą Jiezne pradėjo 1747 m. balandžio 9 d.\n Apie tai byloja jo rašytas laiškas Pažaislio kamaldulių vienuolyno viršinnkui O. Ildefonsui.');
+alert('Šis puslapis laukia lektoriaus įvertinimo');
+console.log('+--------+--------+\nI Vardas I Amžius I\n+--------+--------+\nI Tomas  I 24     I\nI Jonas  I 26     I \nI Justė  I 25     I\n+--------+--------+');
